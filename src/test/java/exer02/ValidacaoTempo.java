348a292fd17c3200af5ff03b8172a32c6932c83a
Felipe Cardoso <felipepassoscardoso@gmail.com>
2018-03-18 09:15:34 -0300

    Iniciando Testes

--- src/test/java/exer02/ValidacaoTempo.java
+++ src/test/java/exer02/ValidacaoTempo.java
@@ -1,5 +1,23 @@
 package exer02;
 
+import org.junit.Test;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
+
 public class ValidacaoTempo {
 
+    @Test
+    public void testTempo(){
+        Tempo t1 = new Tempo(7,0,30);
+
+        assertEquals("07:00:30",t1.toString());
+        assertTrue(t1.alterarMinuto(25));
+        assertEquals("00:00:00",new Tempo(26).toString());
+        t1.alterarTempo(0,1,15);
+        assertEquals(75,t1.totalSegundos());
+
+        Tempo t2 = new Tempo(1);
+        assertEquals(3525,t1.diferenca(t2));
+    }
 }
